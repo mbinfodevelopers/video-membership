dc186eb686b64961e57a6dd670791ce28cc6a5ad
khademmilad <khademmilad@gmail.com>
2022-07-17 11:27:23 +0200

    added postgres to the project

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -62,12 +62,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'video_membership',
+        'USER': 'mbdevelopers',
+        'PASSWORD': 'snap123@Q',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
