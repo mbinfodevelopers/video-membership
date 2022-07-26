c629cefd1d0185cbbda16cbd9d0da79f36add1b5
khademmilad <khademmilad@gmail.com>
2022-07-26 12:05:41 +0200

    improved login part

--- src/account/urls.py
+++ src/account/urls.py
@@ -17,6 +17,6 @@ urlpatterns = [
     path("register/", register_view, name="register"),
     path("logout", logout_view, name='logout'),
     path('<user_id>/edit/', edit_account_view, name="edit"),
-    path('<user_id>/profile/', profile_view, name='profile')
+    path('profile/<user_id>/', profile_view, name='profile')
 
 ]
