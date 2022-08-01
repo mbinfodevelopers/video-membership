c9cf6bade1ba5b17a8b98e05fa0f9068cb1eb4a6
khademmilad <khademmilad@gmail.com>
2022-08-01 17:11:19 +0200

    added video filepath

--- src/course/models.py
+++ src/course/models.py
@@ -48,13 +48,16 @@ class Course(models.Model):
     #     verbose_name = 'course',
     #     verbose_name_plural ='course_section'
 
+def get_video_filepath(self, filename):
+    course_id = self.course.pk
+    return f'videos/{course_id}/{course_id}.mp4'
 
 class Video(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=150, unique=True)
     description = models.TextField(blank=True, null=True)
     slug = models.SlugField(max_length=200, unique=True)
-    video = models.FileField(null=True, blank=True)
+    video = models.FileField(null=True, blank=True, upload_to=get_video_filepath)
     create_date = models.DateTimeField(auto_now_add=True)
     modify_date = models.DateTimeField(auto_now=True)
 
