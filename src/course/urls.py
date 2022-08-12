1e8d680ab3b8d8df737db6bb7369870749358777
mbinfodevelopers <bhman.g2016@gmail.com>
2022-08-12 14:58:21 +0430

    course_detail branch added two

--- src/course/urls.py
+++ src/course/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.all_courses, name="all_courses"),
+    path('', views.all_courses, name="courses"),
     path('<slug:category_slug>/', views.all_courses, name="course_by_category"),
     path('<slug:category_slug>/<slug:course_slug>/', views.course_detail, name="course_detail"),
 ]
