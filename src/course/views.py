76ee5c1e06de7560c6fe3fa5d464f9e37a905e4a
khademmilad <khademmilad@gmail.com>
2022-08-15 16:31:12 +0200

    added intro course video into views and template

--- src/course/views.py
+++ src/course/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, get_object_or_404,HttpResponse
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from home.models import Category
-from .models import Course
+from .models import Course, Video
 
 
 def all_courses(request, category_slug=None):
@@ -20,12 +20,20 @@ def all_courses(request, category_slug=None):
 
 
 def course_detail(request, category_slug, course_slug):
+    context = {}
+
+    # return objects from course model
     try:
         single_course = Course.objects.get(category__slug=category_slug, slug=course_slug)
     except Exception as e:
         raise e
-    context = {
-        'single_course': single_course
-    }
+    context['single_course'] = single_course
+
+    # return videos from course
+    if single_course:
+        videos = Video.objects.filter(course_id=single_course.id)
+        if videos:
+            context['course_intro'] = videos[0]
+            context['course_videos'] = videos
 
     return render(request, 'course/course_detail.html', context)
