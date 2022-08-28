457ecfdeaff4dff13b2f0a2aa56c23823bbe401a
khademmilad <khademmilad@gmail.com>
2022-08-28 22:07:45 +0200

    order part 5 (create and test)

--- src/order/views.py
+++ src/order/views.py
@@ -6,6 +6,23 @@ import json
 
 def order(request):
     context = {}
+    if request.user.is_authenticated:
+        current_user = request.user
+        order, created = Order.objects.get_or_create(user=current_user, is_paid=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        # if user is not authenticated (fix later)
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartItems = order['get_cart_items']
+
+    # fix later
+    print('cartItems', cartItems)
+
+
+
+    context['cartItems'] = cartItems
     return render(request, 'store/order.html', context)
 
 
