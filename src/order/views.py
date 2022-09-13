7f08f57d821ee60f0836eaeef0f2e5915b895c5d
Milad Khadem <56974014+khademmilad@users.noreply.github.com>
2022-09-14 01:26:12 +0200

    Update views.py

--- src/order/views.py
+++ src/order/views.py
@@ -45,24 +45,6 @@ def delete_cart_item(request):
     return redirect('/')
 
 
-# def cart(request):
-#     context = {}
-#     if request.user.is_authenticated:
-#         current_user = request.user
-#
-#         # return all objects from OrderItem's model
-#         order, created = Order.objects.get_or_create(user=current_user, is_paid=False)
-#         items = order.orderitem_set.all()
-#     else:
-#         items = []
-#
-#         # if user is not authenticated (fix later)
-#         order = {'get_cart_total': 0, 'get_cart_items': 0}
-#
-#     context['items'] = items
-#     context['order'] = order
-#     return render(request, 'store/cart.html', context)
-
 
 def checkout(request):
     context = {}
