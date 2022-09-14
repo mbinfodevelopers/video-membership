4de058e265472f891889f75c4086c042c4910f2f
mbinfodevelopers <bhman.g2016@gmail.com>
2022-09-14 23:31:48 +0430

    Change one filed in model file

--- src/account/admin.py
+++ src/account/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import Account, Teacher, Student, Skill
 
-admin.site.register(Account)
-admin.site.register(Student)
-admin.site.register(Teacher)
-admin.site.register(Skill)
+
+class AdminAccount(admin.ModelAdmin):
+    list_display = ['email', 'userAccount_image', 'username', 'is_admin', 'is_active', 'is_staff', 'is_superuser', 'when_user_joined',]
+    readonly_fields = ['email', 'password']
+    list_filter = ['email', 'username']
+
+
+class AdminTeacher(admin.ModelAdmin):
+    list_display = ['teacher', 'teacher_name']
+    list_filter = ['teacher', 'teacher_name']
+
+
+class AdminStudent(admin.ModelAdmin):
+    list_display = ['student']
+
+
+class AdminSkill(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Account, AdminAccount)
+admin.site.register(Teacher, AdminTeacher)
+admin.site.register(Student, AdminStudent)
+admin.site.register(Skill, AdminSkill)
