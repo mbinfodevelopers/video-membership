825002e83453aeee711a065eebd130a1e27b5002
khademmilad <khademmilad@gmail.com>
2022-09-14 17:17:52 +0200

    some changes

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 app_name = "blog"
 urlpatterns = [
-    path('blogs/', views.all_blog, name="all_blog"),
+    path('', views.all_blog, name="all_blog"),
     path('category/<slug:slug>', views.category, name="category"),
     path('article/<slug:slug>', views.article_detail, name="article_detail")
 ]
